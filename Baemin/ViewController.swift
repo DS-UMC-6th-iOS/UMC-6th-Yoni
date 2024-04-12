838a5896bd7fc613316544815ab8ebc4edf5058e
kayeon2 <tjsrkdus@gmail.com>
2024-04-13 00:36:24 +0900

    Initial Commit

---
+++ Baemin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Baemin
+//
+//  Created by 선가연 on 4/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
