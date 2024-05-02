909c64c9db4b5b4160c181cc8b1ad439e06259a0
kayeon2 <tjsrkdus@gmail.com>
2024-05-03 01:37:54 +0900

    Delegate 실습

--- Protocol/SecondViewController.swift
+++ Protocol/SecondViewController.swift
@@ -7,11 +7,24 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, ChangeLabelDelegate {
+    func doChange() {
+        previousViewController?.label.text = self.textField.text
+    }
+    
 
+    @IBOutlet weak var textField: UITextField!
+    
+    var previousViewController : ViewController?
+    
+    @IBAction func buttonDismiss(_ sender: Any) {
+        doChange()
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        previousViewController?.delegate = self
         // Do any additional setup after loading the view.
     }
     
