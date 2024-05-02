d6e4af4ab7646c49bead556c64d6619ec9cd34d1
kayeon2 <tjsrkdus@gmail.com>
2024-05-02 22:05:50 +0900

    로그인 화면 - 화면 전환

--- Catstagram/LoginViewController.swift
+++ Catstagram/LoginViewController.swift
@@ -9,21 +9,49 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    var email = String()
+    var password = String()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func emailTextFieldEditingChanged(_ sender: UITextField) {
+        // 옵셔널 -> 값이 있을 수도 없을 수도
+        // 없을 경우 ?? 뒤 값이 입력됨
+        let text = sender.text ?? ""
+        // print(text)
+        self.email = text
+        
     }
-    */
-
+    
+    
+    @IBAction func passwordTextFieldEditingChanged(_ sender: UITextField) {
+        
+        let text = sender.text ?? ""
+        self.password = text
+    }
+    
+    @IBAction func loginButtonDidTap(_ sender: UIButton) {
+        
+        
+    }
+    
+    @IBAction func registerButtonDidTap(_ sender: UIButton) {
+        // 화면 전환
+        // 1. 스토리보드 생성
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        // 2. 뷰 컨트롤러 생성
+        let registerViewController = storyboard.instantiateViewController(withIdentifier: "RegisterVC") as! RegisterViewController
+        
+        // 3. 화면 전환 메소드 이용해서 화면을 전환
+//        self.present(registerViewController, animated: true, completion: nil)
+        self.navigationController?.pushViewController(registerViewController, animated: true)
+    }
 }
