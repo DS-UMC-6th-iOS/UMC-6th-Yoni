2d916fd37d70a8609d82fc59474db1de9a84720d
kayeon2 <tjsrkdus@gmail.com>
2024-05-03 14:15:14 +0900

    홈 화면 - 피드 레이아웃 구성

--- Catstagram/Home/Cell/FeedTableViewCell.swift
+++ Catstagram/Home/Cell/FeedTableViewCell.swift
@@ -9,9 +9,52 @@ import UIKit
 
 class FeedTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var imageViewUserProfile: UIImageView!
+    @IBOutlet weak var labelUserName: UILabel!
+    @IBOutlet weak var imageViewFeed: UIImageView!
+    @IBOutlet weak var buttonIsHeart: UIButton!
+    @IBOutlet weak var buttonIsBookMark: UIButton!
+    @IBOutlet weak var labelHowManyLike: UILabel!
+    @IBOutlet weak var labelFeed: UILabel!
+    @IBOutlet weak var imageViewMyProfile: UIImageView!
+    
+    // 좋아요 버튼
+    @IBAction func actionIsHeart(_ sender: Any) {
+        if buttonIsHeart.isSelected {
+            buttonIsHeart.isSelected = false
+        }
+        else {
+            buttonIsHeart.isSelected = true
+        }
+    }
+    
+    // 북마크 버튼
+    @IBAction func actionIsBookMark(_ sender: Any) {
+        if buttonIsBookMark.isSelected {
+            buttonIsBookMark.isSelected = false
+        }
+        else {
+            buttonIsBookMark.isSelected = true
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        // 프로필 이미지 둥글게 만들기
+        imageViewUserProfile.layer.cornerRadius = 12.5
+        imageViewUserProfile.clipsToBounds = true
+        
+        imageViewMyProfile.layer.cornerRadius = 12.5
+        imageViewMyProfile.clipsToBounds = true
+        
+        // 폰트
+        let fontSize = UIFont.boldSystemFont(ofSize: 9)
+        let atrributedStr = NSMutableAttributedString(string: labelFeed.text ?? "")
+        atrributedStr.addAttribute(.font, value: fontSize, range: NSRange.init())
+        labelFeed.attributedText = atrributedStr
+        
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- Catstagram/Home/HomeViewController.swift
+++ Catstagram/Home/HomeViewController.swift
@@ -9,21 +9,41 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // 구분선 없애기
+        tableView.separatorStyle = .none
+        
+        // TableView에 등록
+        let feedNib = UINib(nibName: "FeedTableViewCell", bundle: nil)
+        tableView.register(feedNib, forCellReuseIdentifier: "FeedTableViewCell")
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension HomeViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FeedTableViewCell", for: indexPath) as? FeedTableViewCell else { return UITableViewCell()
+        }
+        
+        // 선택 기능 없애기
+        cell.selectionStyle = .none
+        
+        return cell
     }
-    */
-
+    
+    // 크기 설정
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 600
+    }
+    
 }
