e96a5345080ea6babbb5604e8ae0271f57915983
kayeon2 <tjsrkdus@gmail.com>
2024-06-28 02:40:37 +0900

    마이페이지 화면 구성

---
+++ Catstagram/Profile/Cell/PostCell/PostCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PostCollectionViewCell.swift
+//  Catstagram
+//
+//  Created by 선가연 on 6/28/24.
+//
+
+import UIKit
+
+class PostCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
