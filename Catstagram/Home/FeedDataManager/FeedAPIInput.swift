ab7721219ea0e7dd5cf606f5576dd7d958f3a13b
kayeon2 <tjsrkdus@gmail.com>
2024-06-28 15:51:57 +0900

    CatAPI - 홈 화면 피드 완성

---
+++ Catstagram/Home/FeedDataManager/FeedAPIInput.swift
@@ -0,0 +1,8 @@
+//
+//  FeedAPIInput.swift
+//  Catstagram
+//
+//  Created by 선가연 on 6/28/24.
+//
+
+import Foundation
---
+++ Catstagram/Home/FeedDataManager/FeedModel.swift
@@ -0,0 +1,8 @@
+//
+//  FeedModel.swift
+//  Catstagram
+//
+//  Created by 선가연 on 6/28/24.
+//
+
+import Foundation
