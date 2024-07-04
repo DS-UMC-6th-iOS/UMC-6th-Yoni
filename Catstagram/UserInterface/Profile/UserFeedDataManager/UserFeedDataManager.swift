60e2e0110f3f2c8cd346f61e355667f5d8e8dbf1
kayeon2 <tjsrkdus@gmail.com>
2024-07-05 03:16:53 +0900

    Instagram API - 마이페이지 피드 완성

---
+++ Catstagram/UserInterface/Profile/UserFeedDataManager/UserFeedDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  UserFeedDataManager.swift
+//  Catstagram
+//
+//  Created by 선가연 on 7/5/24.
+//
+
+import Foundation
---
+++ Catstagram/UserInterface/Profile/UserFeedDataManager/UserFeedModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserFeedModel.swift
+//  Catstagram
+//
+//  Created by 선가연 on 7/5/24.
+//
+
+import Foundation
