2eb4e81bd5228790336f21043f717f44afc4fe01
kayeon2 <tjsrkdus@gmail.com>
2024-07-07 02:29:58 +0900

    릴스 화면 구성 및 스크롤 처리

---
+++ Catstagram/ReelsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ReelsCollectionViewCell.swift
+//  Catstagram
+//
+//  Created by 선가연 on 7/5/24.
+//
+
+import UIKit
+
+class ReelsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Catstagram/VideoPlayerView.swift
@@ -0,0 +1,8 @@
+//
+//  VideoPlayerView.swift
+//  Catstagram
+//
+//  Created by 선가연 on 7/7/24.
+//
+
+import Foundation
