a938f61d6fd8f2a7fdb1c5a5c16ac0404530cffa
kayeon2 <tjsrkdus@gmail.com>
2024-07-07 23:56:52 +0900

    장바구니 화면 구성, 데이터 전달

---
+++ Baemin/CartTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CartTableViewCell.swift
+//  Baemin
+//
+//  Created by 선가연 on 7/7/24.
+//
+
+import UIKit
+
+class CartTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
