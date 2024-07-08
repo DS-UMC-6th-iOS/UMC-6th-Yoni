1465b351a30c26e97824203877566294be2111ab
kayeon2 <tjsrkdus@gmail.com>
2024-07-09 02:32:36 +0900

    회원가입 화면 구성

--- SocialLogin/ViewController.swift
+++ SocialLogin/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var loginWithEmailBtn: UIButton!
     
+    @IBOutlet weak var registerBtn: UIButton!
+    
     // 카카오 로그인
     @IBAction func LoginWithKakaoDidTap(_ sender: UIButton) {
         // 카카오톡 앱으로 로그인
@@ -52,13 +54,14 @@ class ViewController: UIViewController {
                 print(error)
             } else {
                 print("me() suceess.")
-
+                
                 // 메인 화면으로 이동
                 let storyboard = UIStoryboard(name: "Main", bundle: nil)
                 let mainVc = storyboard.instantiateViewController(withIdentifier: "MainVC") as? MainViewController
                 mainVc?.nickname = user?.kakaoAccount?.profile?.nickname
                 self.view.window?.windowScene?.keyWindow?.rootViewController = mainVc
-            }}
+            }
+        }
     }
     
     override func viewDidLoad() {
@@ -66,10 +69,8 @@ class ViewController: UIViewController {
         
         loginWithEmailBtn.layer.cornerRadius = 5
         loginWithEmailBtn.layer.masksToBounds = true
-        
-        // Do any additional setup after loading the view.
+        registerBtn.layer.cornerRadius = 5
+        registerBtn.layer.masksToBounds = true
     }
-
-
 }
 
